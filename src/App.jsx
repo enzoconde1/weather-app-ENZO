6d45df459d369b3049252586b1770ec36ee76e94
enzoconde1 <enzofconde@outlook.com.ar>
2022-03-10 02:31:33 -0300

    agregado de react-router-dom version espc 5.1 limpieza de archivos preexistentes agregado de carpeta pages con sus links agregado de app renovada

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+    BrowserRouter as Router, Route,
+    Switch
+} from "react-router-dom";
+import CiudadPage from "./pages/CiudadPage";
+import MainPage from "./pages/MainPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import WelcomePage from "./pages/WelcomePage";
+
+const App = () => {
+    return (
+    <div>
+        <h1>App</h1>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <WelcomePage/>
+                </Route>
+                <Route path="/main">
+                    <MainPage/>
+                </Route>
+                <Route path="/city">
+                    <CiudadPage/>
+                </Route>
+                <Route>
+                   <NotFoundPage/>
+                </Route>
+            </Switch>
+        </Router>
+    </div>
+    );
+};
+
+export default App;
---
+++ src/pages/CiudadPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const CiudadPage = () => {
+    return (
+        <div> 
+            Ciudad
+            <div>
+                <Link to='/main'> Volver a Main </Link>
+            </div>
+        </div>
+    )
+}
+
+export default CiudadPage
